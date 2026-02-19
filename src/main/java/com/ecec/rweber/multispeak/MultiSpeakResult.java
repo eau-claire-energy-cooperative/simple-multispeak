fc659704348bc257bd3929cb56f405bb51cfc1e7
Rob Weber <rweber@ecec.com>
2026-02-19 13:53:12 -0600

    added some javadoc strings

--- src/main/java/com/ecec/rweber/multispeak/MultiSpeakResult.java
+++ src/main/java/com/ecec/rweber/multispeak/MultiSpeakResult.java
@@ -10,6 +10,12 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.Namespace;
 
+/**
+ * Represents the returned results from a Multispeak endpoint call. If the method returns a payload the XML can be accessed. If the result returned an error the exception can be viewed. 
+ * 
+ * @author rweber
+ *
+ */
 public class MultiSpeakResult {
 	private Document xml = null;
 	private MultiSpeakException exception = null;
@@ -25,11 +31,17 @@ public class MultiSpeakResult {
 		this.method = method;
 	}
 	
+	/**
+	 * @return if the method call returned a valid XML result
+	 */
 	public boolean isSuccess() {
 		// return true if xml is present
 		return this.xml != null;
 	}
 	
+	/**
+	 * @return the payload body if isSuccess() is true, otherwise NULL
+	 */
 	public Element getResult(){
 		Element result = null;
 		
@@ -82,6 +94,9 @@ public class MultiSpeakResult {
 		return result;
 	}
 	
+	/**
+	 * @return the payload header
+	 */
 	public Map<String,String> getHeader(){
 		Map<String,String> result = new HashMap<String,String>();
 		
@@ -107,6 +122,9 @@ public class MultiSpeakResult {
 		return result;
 	}
 	
+	/**
+	 * @return true if the payload body has any data, false if empty
+	 */
 	public boolean containsData() {
 		boolean result = true;
 		
@@ -121,6 +139,9 @@ public class MultiSpeakResult {
 		return result;
 	}
 	
+	/**
+	 * @return the exception if isSuccess() is false, otherwise NULL
+	 */
 	public MultiSpeakException getException() {
 		return this.exception;
 	}
